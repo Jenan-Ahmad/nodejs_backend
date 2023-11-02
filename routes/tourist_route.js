8e0c265c19d6945f04fb11b63f3f9b4175b777c2
Jenan-Ahmad <69717588+Jenan-Ahmad@users.noreply.github.com>
2023-11-02 19:03:38 +0200

    Add files via upload

--- routes/tourist_route.js
+++ routes/tourist_route.js
@@ -15,7 +15,7 @@ touristRouter.post('/login', TouristController.login);
 
 touristRouter.post('/send-reset-email', TouristController.resetPassword);
 
-touristRouter.post('/edit-account', upload.single('profileImage'), TouristController.updateProfile);
+touristRouter.post('/edit-account', TouristController.updateProfile);
 
 touristRouter.get('/', (req, res) => {
     res.status(200).json({ status: true, success: "connected now" });
