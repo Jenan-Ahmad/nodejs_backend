98cc8cab4fa1b1cc8255f7e888bf98c20e39e895
Jenan-Ahmad <69717588+Jenan-Ahmad@users.noreply.github.com>
2023-11-03 16:52:12 +0200

    Add files via upload

--- controllers/tourist_controller.js
+++ controllers/tourist_controller.js
@@ -178,7 +178,7 @@ exports.updateProfile = async (req, res, next) => {
       console.log("four");
       blobStream.on("finish", async () => {
         console.log("five");
-        const imageUrl = `https://firebasestorage.googleapis.com/v0/b/touristine-9a51a.appspot.com/o/${bucket.name}/${blob.name}?alt=media&token=${metadata.metadata.firebaseStorageDownloadTokens}`;
+        const imageUrl = `https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/${blob.name}?alt=media&token=${metadata.metadata.firebaseStorageDownloadTokens}`;
         console.log("six");
         const { firstName, lastName, password } = req.body;
 
