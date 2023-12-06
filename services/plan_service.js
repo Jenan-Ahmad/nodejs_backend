301ea8744af2d69520ccc43112f9b6896fde4e69
Jenan-Ahmad <69717588+Jenan-Ahmad@users.noreply.github.com>
2023-12-07 01:40:59 +0200

    Add files via upload

--- services/plan_service.js
+++ services/plan_service.js
@@ -302,9 +302,117 @@ class PlanService {
         }
         return planDestinations;
     }
-    
-    static async enlargePlan(){
-            
+
+    static async enlargePlan(planDuration, tripDuration, planDestinations, destinationsByCategory, touristCategories, startTime, endTime) {
+        console.log("-------------------------------enlarge plan");
+        var crntTime = convertTimeStringToDate(startTime).getHours();//holds time scrolling through plan destinations
+        //shuffle places
+        console.log("before shuffle");
+        for (const destination of planDestinations) {
+            console.log("--------------------------", destination.name);
+        }
+        planDestinations = lodash.shuffle(planDestinations);
+        console.log("after shuffle");
+        for (const destination of planDestinations) {
+            console.log("--------------------------", destination.name);
+        }
+        //check timings for each place
+        for (let i = 0; i < planDestinations.length; i++) {
+            const destination = planDestinations[i];
+            if (crntTime >= convertTimeStringToDate(destination.workingHours.openingTime).getHours()
+                && crntTime + destination.estimatedDuration.displayedDuration <= convertTimeStringToDate(destination.workingHours.closingTime).getHours()) {
+                crntTime += destination.estimatedDuration.displayedDuration;
+                console.log("Destination", destination.name, "is saved");
+            } else {
+                let solved = 0;
+                //if problem, find a successor that works from the plan
+                console.log("Destiantion", destination.name, "isn't open at", crntTime);
+                for (let j = i; j < planDestinations.length; j++) {
+                    const solDestination = planDestinations[j];
+                    if (crntTime >= convertTimeStringToDate(solDestination.workingHours.openingTime).getHours()
+                        && crntTime + solDestination.estimatedDuration.displayedDuration <= convertTimeStringToDate(solDestination.workingHours.closingTime).getHours()) {
+                        planDestinations[i] = solDestination;
+                        planDestinations[j] = destination;
+                        console.log("Alter, from plan, Destination", solDestination.name, "is saved");
+                        solved = 1;
+                        crntTime += solDestination.estimatedDuration.displayedDuration;
+                        break;
+                    }
+                }
+                //if not found, check from its category
+                if (solved === 0) {
+                    for (let j = 0; j < destinationsByCategory[destination.category].length; j++) {
+                        const solDestination = destinationsByCategory[destination.category][j];
+                        if (crntTime >= convertTimeStringToDate(solDestination.workingHours.openingTime).getHours()
+                            && crntTime + solDestination.estimatedDuration.displayedDuration <= convertTimeStringToDate(solDestination.workingHours.closingTime).getHours()) {
+                            planDestinations[i] = solDestination;
+                            console.log("Alter, from category, Destination", solDestination.name, "is saved");
+                            solved = 1;
+                            crntTime += solDestination.estimatedDuration.displayedDuration;
+                            break;
+                        }
+                    }
+                }
+                //if still, choose from other categories
+                if (solved === 0) {
+                    for (const category in destinationsByCategory) {
+                        if (touristCategories.includes(category)) {
+                            const categoryArray = destinationsByCategory[category];
+                            for (let j = 0; j < categoryArray.length; j++) {
+                                const solDestination = categoryArray[j];
+                                if (planDestinations.includes(solDestination)) {
+                                    continue;
+                                }
+                                if (crntTime >= convertTimeStringToDate(solDestination.workingHours.openingTime).getHours()
+                                    && crntTime + solDestination.estimatedDuration.displayedDuration <= convertTimeStringToDate(solDestination.workingHours.closingTime).getHours()) {
+                                    planDestinations[i] = solDestination;
+                                    console.log("Alter, from chosen categories, Destination", solDestination.name, "is saved");
+                                    solved = 1;
+                                    crntTime += solDestination.estimatedDuration.displayedDuration;
+                                    break;
+                                }
+                            }
+                        }
+                        if (solved === 1) {
+                            break;
+                        }
+                    }
+                }
+                if (solved === 0) {
+                    for (const category in destinationsByCategory) {
+                        if (!touristCategories.includes(category)) {
+                            const categoryArray = destinationsByCategory[category];
+                            for (let j = 0; j < categoryArray.length; j++) {
+                                const solDestination = categoryArray[j];
+                                if (planDestinations.includes(solDestination)) {
+                                    continue;
+                                }
+                                if (crntTime >= convertTimeStringToDate(solDestination.workingHours.openingTime).getHours()
+                                    && crntTime + solDestination.estimatedDuration.displayedDuration <= convertTimeStringToDate(solDestination.workingHours.closingTime).getHours()) {
+                                    planDestinations[i] = solDestination;
+                                    console.log("Alter, from non-chosen categories, Destination", solDestination.name, "is saved");
+                                    solved = 1;
+                                    crntTime += solDestination.estimatedDuration.displayedDuration;
+                                    break;
+                                }
+                            }
+                        }
+                        if (solved === 1) {
+                            break;
+                        }
+                    }
+                }
+                if (solved === 0) {
+                    planDestinations[i] = null;
+                    planDestinations[i] = { name: "break;", time: 1 };
+                    crntTime += 1;
+                }
+            }
+        }
+        //get amount of left time
+        var leftTime = convertTimeStringToDate(endTime) - crntTime;
+        
+        return planDestinations;
 
     }
 }
