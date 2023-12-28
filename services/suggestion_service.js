aeddba32568a9b99579ff4c3f5c894f9c2447f0f
Jenan-Ahmad <69717588+Jenan-Ahmad@users.noreply.github.com>
2023-12-29 01:55:58 +0200

    Add files via upload

--- services/suggestion_service.js
+++ services/suggestion_service.js
@@ -77,6 +77,7 @@ class SuggestionService {
                     $set: {
                         "comment.reply": adminComment,
                         "comment.admin": email,
+                        "status": 'Seen',
                     },
                 }
             );
